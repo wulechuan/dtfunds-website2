505350512df0638fecb5654f758b5b490045e99d
wulechuan <wulechuan@live.com>
2016-09-11 06:12:25 +0800

    company page done

--- app-dev/scripts/pages/about-company.js
+++ app-dev/scripts/pages/about-company.js
@@ -1,52 +1,232 @@
 (function () {
 	(function processSectionFirstFoldDetailsSwipers() {
-		function buildSlideDoms() {
-	    	// var graphicSlidesRoot = $(graphicSlidesRootSelector)[0];
+		function buildSlideDomsAccordintToTexts(textSlideElements, graphicSlideElements) {
+			var slidesWrapperElement = $(graphicSlidesRootSelector + ' .swiper-wrapper')[0];
+			// console.log(slidesWrapperElement);
+
+			var rulerSubdiv = 5;
+			var lastBuiltYear = NaN;
+			var lastBuiltMonth = NaN;
+			var hasComeToFutureMonths = false;
+
+			var today = new Date();
+			var thisMonth = today.getUTCMonth() + 1;
+			var thisYear = today.getUTCFullYear();
+			// console.log(thisYear, thisMonth);
+			var lastTextSlideElement = null;
+			var letEventsLastTillNextEvent = false;
+			var doNotBuildAnyGapMonths = false;
+
+			for (var i = 0; i < textSlideElements.length; i++) {
+				var currentTextSlideElement = textSlideElements[i];
+				if (!currentTextSlideElement) continue;
+
+
+				var vSpans = $(currentTextSlideElement).find('.value');
+				var month = parseInt(vSpans[1].textContent);
+				if (isNaN(month)) continue;
+
+				var year = parseInt(vSpans[0].textContent);
+				if (isNaN(year)) year = lastBuiltYear;
+				if (isNaN(year)) continue;
+
+				var isDifferentYear = isNaN(lastBuiltYear) || (year !== lastBuiltYear);
+				var isTheSameMonthAsLast = !isDifferentYear && month === lastBuiltMonth;
+
+				if (isNaN(lastBuiltMonth)) lastBuiltMonth = month;
+				if (isNaN(lastBuiltYear)) lastBuiltYear = year;
+
+				var monthGap = (year - lastBuiltYear) * 12 + month - lastBuiltMonth - 1;
+				console.log('frome:', lastBuiltYear, '-', lastBuiltMonth, 'to', year, '-', month, '\t month gap:', monthGap);
+				console.log(currentTextSlideElement.textContent);
+
+				var shouldBuildGapLeftMonth     = !doNotBuildAnyGapMonths && monthGap > 0;
+				var shouldBuildGapRightMonth    = !doNotBuildAnyGapMonths && monthGap > 1;
+				var shouldBuildGapOmittedMonths = !doNotBuildAnyGapMonths && monthGap > 2;
+
+				var yearToBuild, monthToBuild, isThisMonth, isFirstFutureMonth;
+				var builtSlideElement;
+
+				var isFuture = false;
+
+
+				if (shouldBuildGapLeftMonth) {
+					yearToBuild = lastBuiltYear;
+					monthToBuild = lastBuiltMonth + 1;
+					if (monthToBuild > 12) {
+						yearToBuild++;
+						monthToBuild -= 12;
+					}
+
+					isThisMonth = (yearToBuild === thisYear && monthToBuild === thisMonth);
+					isFuture = isFuture || yearToBuild > thisYear || (yearToBuild === thisYear && monthToBuild > thisMonth);
+					isFirstFutureMonth = isFuture && !hasComeToFutureMonths;
+					if (isFuture) hasComeToFutureMonths = true;
+
+					builtSlideElement = _buildOneSlide(slidesWrapperElement, yearToBuild, monthToBuild, isThisMonth, true, false);
+					if (letEventsLastTillNextEvent) builtSlideElement.textSlideElement = lastTextSlideElement;
+					graphicSlideElements.push(builtSlideElement);
+
+					lastBuiltYear = yearToBuild;
+					lastBuiltMonth = monthToBuild;
+				}
+
+
+				if (shouldBuildGapOmittedMonths) {
+					builtSlideElement = _buildOneSlide(slidesWrapperElement, NaN, NaN, false, true, true);
+					if (letEventsLastTillNextEvent) builtSlideElement.textSlideElement = lastTextSlideElement;
+					graphicSlideElements.push(builtSlideElement);
+				}
+
+
+				if (shouldBuildGapRightMonth) {
+					yearToBuild = year;
+					monthToBuild = month - 1;
+					if (monthToBuild < 1) {
+						yearToBuild--;
+						monthToBuild += 12;
+					}
+
+					isThisMonth = (yearToBuild === thisYear && monthToBuild === thisMonth);
+					isFuture = isFuture || yearToBuild > thisYear || (yearToBuild === thisYear && monthToBuild > thisMonth);
+					isFirstFutureMonth = isFuture && !hasComeToFutureMonths;
+					if (isFuture) hasComeToFutureMonths = true;
+
+
+					builtSlideElement = _buildOneSlide(slidesWrapperElement, yearToBuild, monthToBuild, isThisMonth, true, false);
+					if (letEventsLastTillNextEvent) builtSlideElement.textSlideElement = lastTextSlideElement;
+					graphicSlideElements.push(builtSlideElement);
+
+					lastBuiltYear = yearToBuild;
+					lastBuiltMonth = monthToBuild;
+				}
+
+
+				yearToBuild = year;
+				monthToBuild = month;
+
+				isThisMonth = (yearToBuild === thisYear && monthToBuild === thisMonth);
+				isFuture = isFuture || yearToBuild > thisYear || (yearToBuild === thisYear && monthToBuild > thisMonth);
+				isFirstFutureMonth = isFuture && !hasComeToFutureMonths;
+				if (isFuture) hasComeToFutureMonths = true;
+
+				builtSlideElement = _buildOneSlide(slidesWrapperElement, yearToBuild, monthToBuild, isThisMonth, false, false);
+				builtSlideElement.textSlideElement = currentTextSlideElement;
+				graphicSlideElements.push(builtSlideElement);
+
+				lastBuiltYear = yearToBuild;
+				lastBuiltMonth = monthToBuild;
+
+				lastTextSlideElement = currentTextSlideElement;
+				// console.log('======');
+			}
 		}
+
+		function _buildOneSlide(parentElement, year, month, isForComingMonth, isForEmptyMonth, isForOmittedMonths) {
+			var slideElement = document.createElement('div');
+			slideElement.className = 'swiper-slide';
+			slideElement.year = year;
+
+			var innerHTML = [
+				_generateOneRulerSegmentHTML(year, month, isForComingMonth, isForEmptyMonth, isForOmittedMonths, false)
+			];
+
+			if (isForComingMonth) {
+				innerHTML.push(
+					_generateOneRulerSegmentHTML(NaN, NaN, false, true, false, true)
+				);
+			}
+
+			slideElement.innerHTML = innerHTML.join('');
+
+			parentElement.appendChild(slideElement);
+
+			return slideElement;
+		}
+
+		function _generateOneRulerSegmentHTML(year, month, isForComingMonth, isForEmptyMonth, isForOmittedMonths, isLastMonth) {
+			// console.log('building ruler segment for ', year, month, isForComingMonth);
+
+			var futureLabel = isForComingMonth ? '<p class="tip">敬请期待</p>' : '';
+			var rulerExtraClassNames = ''
+				+ (isForEmptyMonth ? ' empty-content' : '')
+				+ (isLastMonth     ? ' hide-chief-scale-line' : '')
+			;
+
+			var rulerLabelHtml = (!isNaN(year) && !isNaN(month)) ? ('<p data-year="'+year+'" class="label">'+month+'月</p>') : '';
+
+			if (isForOmittedMonths) rulerLabelHtml = '<p class="label">...</p>';
+
+			var rulerSegment = [
+				'<div class="ruler-segment',
+					rulerExtraClassNames,
+				'">',
+					futureLabel,
+					rulerLabelHtml,
+					'<i></i><i></i><i></i><i></i>',
+				'</div>'
+			];
+
+			return rulerSegment.join('');
+		}
+
+		var niddleLabel = $('.company-events .fm-style-timeline .fm-niddle .label')[0];
 
 		var graphicSlidesTransitionDurationInSeconds = 0.6;
 		var graphicSlidesRootSelector = '.company-events .fm-style-timeline .swiper-container.graphic-slides';
-		var articleSlidesRootSelector = '.company-events .fm-style-timeline .swiper-container.explaination-slides';
+		var articleSlidesRootSelector = '.company-events .fm-style-timeline .swiper-container.text-slides';
 
 
-		var initialSlideIndex = 0;
+		var $textSlideElements = $(articleSlidesRootSelector + ' .swiper-slide');
+		var graphicSlideElements = [];
+		buildSlideDomsAccordintToTexts($textSlideElements, graphicSlideElements);
 
-		var $articleSlideElements = $(articleSlidesRootSelector + ' .swiper-slide');
 
-    	buildSlideDoms();
-    	console.log(buildSwiper(), $(graphicSlidesRootSelector + ' .swiper-slide'));
+		var initialSlideIndex = graphicSlideElements.length - 1;
+		var swiperControl = buildSwiper();
+		swiperControl.slideTo(initialSlideIndex);
 
-    	function buildSwiper() {
+
+		function buildSwiper() {
 			return new window.Swiper(graphicSlidesRootSelector, {
 				direction: 'horizontal',
 				speed: graphicSlidesTransitionDurationInSeconds * 1000,
 
-				initial: initialSlideIndex,
 				mousewheelControl: true,
 				slideToClickedSlide: true,
 
 				loop: false,
 				slidesPerView: 'auto',
 				centeredSlides: true,
-				// loopedSlides: 0,
 
 				onSlideChangeStart: function (thisSwiperControl) {
-					console.error('fake codes below');
-					var textSlideIndexToShow = thisSwiperControl.activeIndex;
+					var graphicSlideActiveIndex = thisSwiperControl.activeIndex;
+					var avtiveGraphicSlideElement = thisSwiperControl.slides[graphicSlideActiveIndex];
+
+
+					var year = avtiveGraphicSlideElement.year;
+					if (!isNaN(year) && niddleLabel) {
+						niddleLabel.textContent = avtiveGraphicSlideElement.year;
+					}
+
+
+					var textSlideElementToShow = avtiveGraphicSlideElement.textSlideElement;
+					console.log('year:', year, textSlideElementToShow);
 
 					var cssClassNameActiveArticle = 'active'; 
-					for (var i = 0; i < $articleSlideElements.length; i++) {
-						var article = $articleSlideElements[i]; 
-						if (i === textSlideIndexToShow) { 
-							$(article).addClass(cssClassNameActiveArticle); 
+					for (var i = 0; i < $textSlideElements.length; i++) {
+						var textSlide = $textSlideElements[i];
+
+						if (textSlide === textSlideElementToShow) { 
+							$(textSlide).addClass(cssClassNameActiveArticle); 
 						} else { 
-							$(article).removeClass(cssClassNameActiveArticle);
+							$(textSlide).removeClass(cssClassNameActiveArticle);
 						}
 					}
 
 					// console.log('---------------------------');
 				}
 			});
-    	}
+		}
 	})();
 })();
